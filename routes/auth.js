19cc58bdfbdff24cc316944e4bb496904a853678
Ivan <ivan.zf.netc@gmail.com>
2021-01-12 14:45:28 +0200

    Install Angular - material

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const controller = require('../controllers/auth')
 const router = express.Router()
 
-router.get('/login', (req, res) => {
-  res.status(200).json({
-    login: true
-  })
-})
+router.post('/login', controller.login)
+router.post('/register', controller.register)
+
+router.get('/login', controller.login)
+router.get('/register', controller.register)
 
 module.exports = router
